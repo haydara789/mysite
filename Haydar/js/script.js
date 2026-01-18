511b7d2fdf5afb828391a893dec73b9b6e524dff
Haydar Ahmed Abdelaziz <haydarahmedabdelaziz@gmail.com>
2026-01-18 15:53:45 +0200

    Add files via upload

---
+++ Haydar/js/script.js
@@ -0,0 +1,10 @@
+var age = 10
+if (age >= 70) {
+
+    console.log("this man is old :" + age)
+} else if (age <= 70)
+    console.log("this man is mid :" + age)
+else ((age < 50))
+console.log("this man is young :" + age)
+
+
